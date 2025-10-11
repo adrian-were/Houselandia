49c085695ddd66c51b3514faedf0b30f3b0f94d4
Adrian <adrianshisia@gmail.com>
2025-10-11 21:15:13 +0300

    React app created

---
+++ houselandia/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ houselandia/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <div style={{ backgroundColor: 'lightblue', padding: '20px' }}>
+      <h1>This is the Banner</h1>
+    </div>
+  );
+}
+
+export default Banner;
+
+
